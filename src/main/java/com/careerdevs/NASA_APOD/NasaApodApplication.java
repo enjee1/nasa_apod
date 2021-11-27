634a36b23ac195b8aad8ad27937fecdb1d8b1e2f
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-26 23:23:12 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/NASA_APOD/NasaApodApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.NASA_APOD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaApodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/NASA_APOD/NasaApodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.NASA_APOD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
